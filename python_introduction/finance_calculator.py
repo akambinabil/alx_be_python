4c56659e5a3254b551b6ac622227ed66d6cc4140
LAWANI Akambi Nabil <akambinabil@outlook.com>
2025-05-26 10:16:09 +0100

    Personal Finance Calculator

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,9 @@
+montly_income = float(input("Enter your montly income: "))
+montly_expenses = float(input("Enter your montly expenses: "))
+
+montly_savings = montly_income - montly_expenses
+
+projected_savings = montly_savings * 12 + (montly_savings * 12 * 0.05))
+
+print("Your montly saving are ", montly_savings)
+print("Projected savings after one year, with interest, is: ", projected_savings, ".")
