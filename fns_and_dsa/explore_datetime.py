24141d8ecb839a69b842cd29cc879984cc368894
LAWANI Akambi Nabil <akambinabil@outlook.com>
2025-06-07 09:47:33 +0100

    Explore  Module

---
+++ fns_and_dsa/explore_datetime.py
@@ -0,0 +1,23 @@
+import datetime
+def display_curent_datetime():
+    current_date = datetime.datetime.now()
+    formatted_datetime = current_date.strftime("%Y-%m-%d %H:%M:%S")
+    print(f"Current date and time: {formatted_datetime}")
+
+def calculate_future_date(number_of_days):
+    current_date = datetime.datetime.now()
+    future_date = current_date + datetime.timedelta(days=number_of_days)
+    formatted_future_date = future_date.strftime("%Y-%m-%d")
+    print(f"Future date: {formatted_future_date}")
+
+def main():
+    display_curent_datetime()
+    
+    try:
+        days = int(input("Enter the number of days to add to the current date: "))
+        calculate_future_date(days)
+    except ValueError:
+        print("Please enter a valid integer for the number of days.")
+
+if __name__ == "__main__":
+    main()
